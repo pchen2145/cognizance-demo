24e734645a5fcadbfaa35395e4578daea67f5a97
Peter <46401526+pchen2145@users.noreply.github.com>
2023-06-27 14:48:23 -0400

    Update app.py

--- app/app.py
+++ app/app.py
@@ -14,7 +14,7 @@ Display Price
 def price():
     price = random.randint(2500000, 3500000)/100
     price_string = str(price)
-    return "The price of BTC is: " + price_string
+    return "The latest price of BTC is: " + price_string
 
 """
 Runtime
